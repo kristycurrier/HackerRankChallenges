4933ed6151832f80607f0573602f629b1e25bec8
Kristy <42523857+kristycurrier@users.noreply.github.com>
2019-10-08 14:54:19 -0400

    Created HelloWorld class

---
+++ HelloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp
+{
+    public class HelloWorld
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello world!");
+            Console.ReadKey();
+        }
+    }
+}
